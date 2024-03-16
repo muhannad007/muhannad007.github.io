70d42c5058ad02633c0be308cb97abfac5f3eae7
muhannad007 <muhannadafeefe654@gmail.com>
2024-03-16 13:37:44 +0300

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./muhannad007.github.io/"
+  // base: "./muhannad007.github.io/"
+  base: "./"
 })
